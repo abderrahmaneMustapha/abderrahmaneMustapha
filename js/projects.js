e4b9f93a906e9a9c2136db23bb38d7efb7a8fd6a
abderrahmaneMustapha <abderrahmanemustapha030898@gmail.com>
2020-04-18 20:05:05 +0100

    add skills social and more projects

--- js/projects.js
+++ js/projects.js
@@ -16,23 +16,69 @@ addProjects = ()=>{
     } ,
 
     {
+        'title' : 'Cpu Dash',   
+        'description' : 'this one is about dash that display graphd and a table  showing info about '+
+                        'cpu vendors, models, os, memory and alot more ',
+                       
+        'image' : 'img/dash.gif'            
+    } ,
+
+    {
         'title' : 'Pic Downloader',   
         'description' : 'In this project you can download pictures from pixabay using your command line '+
                         'and search for the downloaded pictures using a pyton interface in this project i used kivy and python ',
                        
         'image' : 'https://raw.githubusercontent.com/abderrahmaneMustapha/python-pixabay-downloader/master/report/images%20and%20gif/ExecutionKivy.gif'            
     } ,
+
+    {
+        'title' : 'Cryptopals',   
+        'description' : 'contain the cryptols challenge solutions its about cryptography i used python to slove 6 challnges '+
+                        'and i will work on java solutions and continue to solve other challnges soon',
+                       
+        'image' : 'img/cryptopals.png'            
+    } ,
+
+    {
+        'title' : 'About malcomX',   
+        'description' : 'Its about malcom X life  this is one of my freecodecamp projects ',
+                       
+        'image' : 'img/malcom.png'            
+    } ,
+
+    {
+        'title' : 'Python Course',   
+        'description' : 'a simple python course one of my freecodecamp projects ',
+                       
+        'image' : 'img/course.png'            
+    } ,
+
+   
+
+    {
+        'title' : 'Pomodoro clock',   
+        'description' : 'a simple pmodoro clock one of my freecodecamp projects ',
+                       
+        'image' : 'img/pomodoro.png'            
+    } ,
+
+    {
+        'title' : 'Assassins creed survey',   
+        'description' : 'an assassins creed surevey template freecodecamp projects ',
+                       
+        'image' : 'img/assassins.png'            
+    } 
 ] 
     
     projects_container = document.getElementById("projects")
     projects.forEach( e => {
         console.log(projects_container)
         projects_container.innerHTML += 
-        '<div class="col-md-4">'+
+        '<div class="col-md-4 mt-4">'+
             '<div class="card">'+            
                 '<h5 class="card-title text-center">'+e['title']+'</h5>'+
                 '<img src='+e['image']+'>'+
-                '<p class="card-text">'+e['description'].substring(0,100)+'...</p>'+            
+                '<p class="card-text p-2">'+e['description'].substring(0,100)+'...</p>'+            
                 '<button class="btn btn-primary" type="button">Learn more</button>'+
             '</div>'+
        '</div>'
