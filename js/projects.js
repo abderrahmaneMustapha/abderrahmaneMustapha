eb98f5cf0622d30e8f9d246fe23e3510a9fafe7a
abderrahmaneMustapha <abderrahmanemustapha030898@gmail.com>
2020-04-23 02:45:35 +0100

    delete border for main and add modal

--- js/projects.js
+++ js/projects.js
@@ -1,10 +1,32 @@
+/* i found this function in  https://gist.github.com/codeguy/6684588 */
+
+slugify  = (str)=> {
+    str = str.replace(/^\s+|\s+$/g, ''); // trim
+    str = str.toLowerCase();
+  
+    // remove accents, swap ñ for n, etc
+    var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
+    var to   = "aaaaeeeeiiiioooouuuunc------";
+    for (var i=0, l=from.length ; i<l ; i++) {
+        str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+    }
+
+    str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+        .replace(/\s+/g, '-') // collapse whitespace and replace by -
+        .replace(/-+/g, '-'); // collapse dashes
+
+    return str;
+}
+
+
 /* add projects  */
 
 addProjects = ()=>{
     let projects = [{
         'title' : 'Learno',   
         'description' : 'this is my first django project, i learned alot from this i tried so much technologies'+
-                        'like redis postegres and celery , ajax , websocket , responsive design',
+                        'like redis postegres and celery , ajax , websocket , responsive design, it was hard some time i didnt even know'+
+                        'what to do but it was fun at the same time ',
         'image' : 'img/learno.jpg'
     },
     {
@@ -86,10 +108,34 @@ addProjects = ()=>{
                 '<img src='+e['image']+'>'+                    
                 '<h5 class="card-title text-center">'+e['title']+'</h5>'+
                 '<p class="card-text p-2">'+e['description'].substring(0,50)+'...</p>'+            
-                '<button data-toggle="modal" data-target="#projectsModal" class="btn btn-primary" type="button">Learn more</button>'+            
+                '<button data-toggle="modal" data-target="#projectsModal'+slugify(e['title'])+'" class="btn btn-primary" type="button">Learn more</button>'+         
+            '</div>'+
+            
+            '<div class="modal fade"  id="projectsModal'+slugify(e['title'])+'" tabindex="" role="dialog" aria-labelledby="Modal'+slugify(e['title'])+'" aria-hidden="true">'+
+                '<div class="modal-dialog  modal-lg" role="document">'+
+                    '<div class="modal-content">'+
+                        '<div class="modal-header text-center">'+
+                           '<h3 class="modal-title" id="exampleModalLongTitle">'+e['title']+'</h3>'+
+                           '<button type="button" class="close" data-dismiss="modal" aria-label="Close">'+
+                            '<span aria-hidden="true">&times;</span>'+
+                            '</button>'+
+                        '</div>'+
+                        '<div class="modal-body text-center p-4">'+
+                            '<img src='+e['image']+'>'+ 
+                            '<p>'+e['description']+' .</p>'+
+                        '</div>'+ 
+                        '<div class="modal-footer">'+
+                            '<a  class="btn btn-secondary text-white" >Project link</a>'+
+                        '</div>'+
+                    '</div>'+
+                    
+                '</div>'+
+
             '</div>'+
        '</div>'
     })
 }
 
 addProjects()
+
+
