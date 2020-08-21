18e691d615bcc3255660c38bccbb87b93d8e8ad1
abderrahmaneMustapha <abderrahmanemustapha030898@gmail.com>
2020-08-21 20:15:28 +0100

    planing for new portfolio

---
+++ new/src/components/navs/mainnav/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+class MainNav extends React.Component{
+
+    render(){
+        return(
+            <nav>
+                
+            </nav>
+        )
+    }
+}
---
+++ new/src/pages/home/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css"
+
+class Home extends React.Component{
+
+    render(){
+        return(
+            <div id="main-home">
+                <h1 className="text-white">Hello there its Toumi Mustapha Abderrahmane</h1>
+                <h5>i'm software engineering student , and a full stack webdeveloper </h5>
+                <h5>passionate and ready to face challenges and solve problems</h5>
+            </div>
+        )
+    }
+}
+
+export default Home
