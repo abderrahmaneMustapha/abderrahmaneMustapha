568fdfb8312803d95c5091bfd5e2d4e805b86a7a
abderrahmane <abderrahmaneMustapha030898@gmail.com>
2020-10-30 20:54:24 +0100

    Updates

---
+++ precache-manifest.fd06d549de7875652ff7e22bce844982.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f9336b9c70bf6016f3405455f7ee32a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae6fab18c137627da273",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "f689ea00dd39cc56ad80",
+    "url": "/static/css/main.29b51758.chunk.css"
+  },
+  {
+    "revision": "ae6fab18c137627da273",
+    "url": "/static/js/2.b0d70521.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b0d70521.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f689ea00dd39cc56ad80",
+    "url": "/static/js/main.5e1fa4d9.chunk.js"
+  },
+  {
+    "revision": "3f3ac9fe654cae80f741",
+    "url": "/static/js/runtime-main.8435eee7.js"
+  }
+]);
